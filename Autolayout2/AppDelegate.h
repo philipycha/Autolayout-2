98642a7d1597d21bedb103149764218a61216289
philipycha <philip.yc.ha@gmail.com>
2016-09-13 14:27:02 -0700

    finished

---
+++ Autolayout2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Autolayout2
+//
+//  Created by Philip Ha on 2016-09-13.
+//  Copyright © 2016 Philip Ha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Autolayout2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Autolayout2
+//
+//  Created by Philip Ha on 2016-09-13.
+//  Copyright © 2016 Philip Ha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
